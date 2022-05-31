0213a4fd35afe9502c20b47b4109c05eed3e1bd9
rizkitirta <rizkibuwono0@gmail.com>
2022-05-31 20:42:08 +0700

    create context

---
+++ context_test.go
@@ -0,0 +1,15 @@
+package golangcontext
+
+import (
+	"context"
+	"fmt"
+	"testing"
+)
+
+func TestContext(t *testing.T) {
+	background := context.Background()
+	fmt.Println(background)
+
+	todo := context.TODO()
+	fmt.Println(todo)
+}
