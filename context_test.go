b7a7aed4a27c0ea4a4ca7573130a48d2738f6fd5
rizkitirta <rizkibuwono0@gmail.com>
2022-05-31 20:46:36 +0700

    context with value

--- context_test.go
+++ context_test.go
@@ -13,3 +13,19 @@ func TestContext(t *testing.T) {
 	todo := context.TODO()
 	fmt.Println(todo)
 }
+
+func TestContextWithValue(t *testing.T) {
+	contextA := context.Background()
+	
+	contextB := context.WithValue(contextA, "b", "A")
+	contextC := context.WithValue(contextA, "c", "C")
+
+	contextD := context.WithValue(contextB, "d", "D")
+	contextE := context.WithValue(contextB,"e", "E")
+
+	fmt.Println(contextA)
+	fmt.Println(contextB)
+	fmt.Println(contextC)
+	fmt.Println(contextD)
+	fmt.Println(contextE)
+}
