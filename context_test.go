6149ba0375316e30951c9aba669010eb39ef4bb9
rizkitirta <rizkibuwono0@gmail.com>
2022-05-31 21:15:30 +0700

    context with cancel

--- context_test.go
+++ context_test.go
@@ -3,6 +3,7 @@ package golangcontext
 import (
 	"context"
 	"fmt"
+	"runtime"
 	"testing"
 )
 
@@ -16,16 +17,53 @@ func TestContext(t *testing.T) {
 
 func TestContextWithValue(t *testing.T) {
 	contextA := context.Background()
-	
+
 	contextB := context.WithValue(contextA, "b", "A")
 	contextC := context.WithValue(contextA, "c", "C")
 
 	contextD := context.WithValue(contextB, "d", "D")
-	contextE := context.WithValue(contextB,"e", "E")
+	contextE := context.WithValue(contextB, "e", "E")
 
 	fmt.Println(contextA)
 	fmt.Println(contextB)
 	fmt.Println(contextC)
 	fmt.Println(contextD)
 	fmt.Println(contextE)
+}
+
+func RunCounter(ctx context.Context) chan int {
+	destination := make(chan int)
+	go func() {
+		defer close(destination)
+		counter := 1
+		for {
+			select {
+			case <-ctx.Done():
+				return
+
+			default:
+				destination <- counter
+				counter++
+			}
+		}
+	}()
+
+	return destination
+}
+
+func TestContextWithCancel(t *testing.T) {
+	fmt.Println("total goroutine", runtime.NumGoroutine())
+
+	ctx, cancel := context.WithCancel(context.Background())
+	destination := RunCounter(ctx)
+
+	for v := range destination {
+		fmt.Println("counter", v)
+		if v == 5 {
+			break
+		}
+	}
+
+	cancel() // Mengirim sinyal cancel ke goroutine yang menjalankan counter
+	fmt.Println("total goroutine", runtime.NumGoroutine())
 }
